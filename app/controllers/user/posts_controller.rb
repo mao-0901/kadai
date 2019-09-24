9ed8ac6f3aab1e89aa623f8a91b8ec2fc112424e
Mao Shimizu <maohir9u1@gmail.com>
2019-09-24 13:35:31 +0900

    add_user/posts

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,6 +1,7 @@
 class User::PostsController < ApplicationController
     #before_action :require_login, only[:index]
     def index
-        @posts = Post.where(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(25)
+        #ポストのユーザーIDを取って、そのユーザーの投稿すべてをとってくる
+        @posts = Post.where(user_id: params[:user_id]).order(created_at: :desc).page(params[:page]).per(25)
     end
 end
